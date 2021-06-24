b5180b41434f86a7371137da7b89bf6baed78979
Marco Roth <marco.roth@intergga.ch>
2021-06-24 17:08:13 +0200

    rename

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -47,8 +47,8 @@ export function activate(context: ExtensionContext) {
 
   // Create the language client and start the client.
   client = new LanguageClient(
-    'languageServerExample',
-    'Language Server Example',
+    'languageServerStimulus',
+    'Stimulus Language Server Example',
     serverOptions,
     clientOptions
   );
--- server/src/server.ts
+++ server/src/server.ts
@@ -116,7 +116,7 @@ function getDocumentSettings(resource: string): Thenable<ExampleSettings> {
   if (!result) {
     result = connection.workspace.getConfiguration({
       scopeUri: resource,
-      section: 'languageServerExample'
+      section: 'languageServerStimulus'
     });
     documentSettings.set(resource, result);
   }
