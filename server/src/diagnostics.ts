4fefdb9ecdabcbf1bd11f3f0f3d80ec5689f2c69
Marco Roth <marco.roth@intergga.ch>
2023-09-09 03:16:22 +0200

    Refactor `Diagnostics` to use `vscode-html-languageservice`

--- server/src/diagnostics.ts
+++ server/src/diagnostics.ts
@@ -1,5 +1,7 @@
-import { Connection, Diagnostic, DiagnosticSeverity } from 'vscode-languageserver/node';
+import { Connection, Diagnostic, DiagnosticSeverity, Range } from 'vscode-languageserver/node';
 import { TextDocument } from 'vscode-languageserver-textdocument';
+import { getLanguageService, Node, LanguageService } from 'vscode-html-languageservice';
+
 import { StimulusHTMLDataProvider } from './data_providers/stimulus_html_data_provider';
 
 export interface InvalidControllerDiagnosticData {
@@ -10,53 +12,98 @@ export class Diagnostics {
   private readonly connection: Connection
   private readonly stimulusDataProvider: StimulusHTMLDataProvider
   private readonly diagnosticsSource = "Stimulus LSP"
+  private diagnostics: Map<TextDocument, Diagnostic[]> = new Map();
 
   constructor(connection: Connection, stimulusDataProvider: StimulusHTMLDataProvider) {
     this.connection = connection;
     this.stimulusDataProvider = stimulusDataProvider;
   }
 
-  get validControllers() {
+  get controllers() {
     return this.stimulusDataProvider.controllers.map((controller) => controller.dasherized);
   }
 
-  validateDataControllerAttributes(textDocument: TextDocument) {
-    const text = textDocument.getText();
-    const pattern = /data-controller=["'](.+)["']/g;
-    const diagnostics: Diagnostic[] = [];
+  visitNode(node: Node, textDocument: TextDocument, service: LanguageService) {
+    const attributes = node.attributes;
 
-    let m: RegExpExecArray | null;
+    if (attributes !== undefined) {
+      Object.keys(attributes).forEach((attribute: string) => {
+        if (attribute === "data-controller") {
+          const quotedIdentifier = attributes[attribute];
+          const identifier = quotedIdentifier ? quotedIdentifier.substr(1, quotedIdentifier.length - 2) : null;
 
-    while ((m = pattern.exec(text))) {
-      const match = m[0];
-      const identifier = m[1];
+          if (quotedIdentifier && identifier && !this.controllers.includes(identifier)) {
+            const range = this.rangeFromNode(textDocument, node);
+            const startTagContent = textDocument.getText(range);
+            const attributeRange = this.rangeForAttribute(textDocument, startTagContent, node, attribute, identifier);
 
-      if (!this.validControllers.includes(identifier)) {
-        const offset = match.indexOf(identifier);
-        const start = m.index + offset;
-        const end = m.index + offset + identifier.length;
-
-        const diagnostic: Diagnostic = {
-          severity: DiagnosticSeverity.Warning,
-          range: {
-            start: textDocument.positionAt(start),
-            end: textDocument.positionAt(end)
-          },
-          message: `${identifier} isn't a valid controller.`,
-          source: this.diagnosticsSource,
-          code: "stimulus.controller.invalid",
-          data: {
-            identifier
+            this.createInvalidControllerDiagnosticFor(identifier, textDocument, attributeRange);
           }
-        };
-
-        diagnostics.push(diagnostic);
-      }
+        }
+      });
     }
+
+    node.children.forEach((child) => {
+      this.visitNode(child, textDocument, service);
+    });
+  }
+
+  validateDataControllerAttributes(textDocument: TextDocument) {
+    const service = getLanguageService();
+    const html = service.parseHTMLDocument(textDocument);
+
+    html.roots.forEach((node: Node) => {
+      this.visitNode(node, textDocument, service);
+    });
+
+    this.sendDiagnosticsFor(textDocument);
+  }
+
+  private rangeFromNode(textDocument: TextDocument, node: Node) {
+    return Range.create(
+      textDocument.positionAt(node.start),
+      textDocument.positionAt(node.startTagEnd || node.end)
+    );
+  }
+
+  private rangeForAttribute(textDocument: TextDocument, tagContent: string, node: Node, attribute: string, value: string) {
+    const attributeStartIndex = tagContent.indexOf(attribute);
+
+    const attributeValueStart = node.start + attributeStartIndex + attribute.length + 2;
+    const attributeValueEnd = attributeValueStart + value.length;
+
+    return Range.create(
+      textDocument.positionAt(attributeValueStart),
+      textDocument.positionAt(attributeValueEnd)
+    );
+  }
+
+  private createInvalidControllerDiagnosticFor(identifier: string, textDocument: TextDocument, range: Range) {
+    const diagnostic: Diagnostic = {
+      severity: DiagnosticSeverity.Warning,
+      range: range,
+      message: `"${identifier}" isn't a valid Stimulus controller.`,
+      source: this.diagnosticsSource,
+      code: "stimulus.controller.invalid",
+      data: {
+        identifier
+      }
+    };
+
+    const diagnostics = this.diagnostics.get(textDocument) || [];
+    diagnostics.push(diagnostic);
+
+    this.diagnostics.set(textDocument, diagnostics);
+  }
+
+  private sendDiagnosticsFor(textDocument: TextDocument) {
+    const diagnostics = this.diagnostics.get(textDocument) || [];
 
     this.connection.sendDiagnostics({
       uri: textDocument.uri,
       diagnostics
     });
+
+    this.diagnostics.delete(textDocument);
   }
 }
