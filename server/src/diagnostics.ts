8fc5c379c141b40c616f249beed687ee7e96fa1d
Marco Roth <marco.roth@intergga.ch>
2023-10-20 11:38:17 +0200

    Update copy for `stimulus.attribute.mismatch` diagnostic

--- server/src/diagnostics.ts
+++ server/src/diagnostics.ts
@@ -371,7 +371,7 @@ export class Diagnostics {
     range: Range
   ) {
     this.pushDiagnostic(
-      `The data attribute for "${valueName}" on the "${identifier}" controller is camelCased, but should be dasherized ("${dasherize(valueName)}"). Please use dashes to separate the Stimulus data attributes.`,
+      `The data attribute for "${valueName}" on the "${identifier}" controller is camelCased, but should be dasherized ("${dasherize(valueName)}"). Please use dashes to for Stimulus data attributes.`,
       "stimulus.attribute.mismatch",
       range,
       textDocument,
