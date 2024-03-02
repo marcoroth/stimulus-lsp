e6d56e5aedf80aa4b52e3cab42cee36b85c38892
Marco Roth <marco.roth@intergga.ch>
2024-03-03 00:26:10 +0100

    Implement `stimulus.controller.register` code action

--- server/src/utils.ts
+++ server/src/utils.ts
@@ -27,3 +27,7 @@ export function camelize(value: string) {
 export function dasherize(value: string) {
   return value.replace(/([A-Z])/g, (_, char) => `-${char.toLowerCase()}`)
 }
+
+export function capitalize(value: string) {
+  return value.charAt(0).toUpperCase() + value.slice(1)
+}
